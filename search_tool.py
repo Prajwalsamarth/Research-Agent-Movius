e23c4b52c54c4509265f3303c75b7c7a432f96de
Prajwal Samarth <49201047+Prajwalsamarth@users.noreply.github.com>
2025-08-17 01:57:38 +0530

    Update search_tool.py

--- search_tool.py
+++ search_tool.py
@@ -55,8 +55,7 @@ def search_web(state: search_state):
     
     # Search
     search_docs = tavily_search.invoke(search_query.search_query)
-    # print('search_query:',search_query.search_query)
-    # print('search_docs:',search_docs)
+
      # Format
     formatted_search_docs = "\n\n---\n\n".join(
         [
@@ -119,91 +118,13 @@ When answering questions, follow these guidelines:
         
 And skip the addition of the brackets as well as the Document source preamble in your citation."""
 
-    # Get state
-    # messages = state["messages"]
-    # context = state["context"]
 
     # Answer question
     system_message = answer_instructions
     answer = llm.invoke([HumanMessage(content=system_message)])
             
-    # Name the message as coming from the expert
-    # answer.name = "expert"
-    
-    # Append it to state
-    # print('answer done')
-    # print("aaaa",answer.content)
-    # print("type a",type(answer.content))
-    # print("bbbb",state["context"])
-    # print("type b",type(state["context"]))
-    # print('answer done')
     return {"context": state["context"] + [answer.content]}
 
-# section_writer_instructions = """You are an expert technical writer. 
-            
-# Your task is to create a short, easily digestible section of a report based on a set of source documents.
-
-# 1. Analyze the content of the source documents: 
-# - The name of each source document is at the start of the document, with the <Document tag.
-        
-# 2. Create a report structure using markdown formatting:
-# - Use ## for the section title
-# - Use ### for sub-section headers
-        
-# 3. Write the report following this structure:
-# a. Title (## header)
-# b. Summary (### header)
-# c. Sources (### header)
-
-# 4. Make your title engaging based upon the focus area of the analyst: 
-# {angle}
-
-# 5. For the summary section:
-# - Set up summary with general background / context related to the focus area of the analyst
-# - Emphasize what is novel, interesting, or surprising about insights gathered.
-# - Create a numbered list of source documents, as you use them
-# - Do not mention the names of interviewers or experts
-# - Aim for approximately 400 words maximum
-# - Use numbered sources in your report (e.g., [1], [2]) based on information from source documents
-        
-# 6. In the Sources section:
-# - Include all sources used in your report
-# - Provide full links to relevant websites or specific document paths
-# - Separate each source by a newline. Use two spaces at the end of each line to create a newline in Markdown.
-# - It will look like:
-
-# ### Sources
-# [1] Link or Document name
-# [2] Link or Document name
-
-# 7. Be sure to combine sources. For example this is not correct:
-
-# [3] https://ai.meta.com/blog/meta-llama-3-1/
-# [4] https://ai.meta.com/blog/meta-llama-3-1/
-
-# There should be no redundant sources. It should simply be:
-
-# [3] https://ai.meta.com/blog/meta-llama-3-1/
-        
-# 8. Final review:
-# - Ensure the report follows the required structure
-# - Include no preamble before the title of the report
-# - Check that all guidelines have been followed"""
-
-# def write_section(state: InterviewState):
-
-#     """ Node to answer a question """
-
-#     # Get state
-#     context = state["context"]
-#     angle = state["angle"]
-   
-#     # Write section using either the gathered source docs from interview (context) or the interview itself (interview)
-#     system_message = section_writer_instructions.format(angle=angle)
-#     section = llm.invoke([SystemMessage(content=system_message)]+[HumanMessage(content=f"Use this source to write your section: {context}")]) 
-                
-#     # Append it to state
-#     return {"sections": [section.content]}
 
 # Add nodes
 search_builder = StateGraph(search_state)
@@ -228,3 +149,4 @@ search_agent = search_builder.compile(checkpointer=memory)
 # search_agent.invoke({"question": "What is the impact of microplastics on marine biodiversity?",
 # "perspective": "Trophic transfer and food-web effects: how microplastics move through feeding interactions from plankton to larger predators, potentially altering feeding efficiency, energy transfer, and predator–prey dynamics."
 # })
+
